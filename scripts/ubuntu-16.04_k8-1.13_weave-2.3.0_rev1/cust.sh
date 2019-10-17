d80b8953b3c798f189426858da55aab2f7988e7e
Andrew Ni <niandrew7@gmail.com>
2019-10-17 14:04:08 -0700

    Added sysctl.conf reload
    Signed-off-by: Andrew Ni <niandrew7@gmail.com>

--- scripts/ubuntu-16.04_k8-1.13_weave-2.3.0_rev1/cust.sh
+++ scripts/ubuntu-16.04_k8-1.13_weave-2.3.0_rev1/cust.sh
@@ -5,6 +5,7 @@ set -e
 echo 'net.ipv6.conf.all.disable_ipv6 = 1' >> /etc/sysctl.conf
 echo 'net.ipv6.conf.default.disable_ipv6 = 1' >> /etc/sysctl.conf
 echo 'net.ipv6.conf.lo.disable_ipv6 = 1' >> /etc/sysctl.conf
+sudo sysctl -p
 echo 'nameserver 8.8.8.8' >> /etc/resolvconf/resolv.conf.d/tail
 resolvconf -u
 systemctl restart networking.service
